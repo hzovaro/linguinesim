9fd7ea13c70c70e266115e14b3166affe8eeacf1
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-04 16:31:46 +1000

    Modified to reflect module changes

--- scripts/li_sim.py
+++ scripts/li_sim.py
@@ -3,7 +3,6 @@
 # 	File:		li_sim.py
 #	Author:		Anna Zovaro
 #	Email:		anna.zovaro@anu.edu.au
-#	Edited:		29/06/2016
 #
 #	Description:
 #	Generating some simulated images of the lucky imaging process. Not worrying about 
@@ -30,7 +29,7 @@
 #
 ############################################################################################
 from __future__ import division
-from apd_sim import *
+from apdsim import *
 plt.close('all')
 
 N_exp = 5
@@ -39,14 +38,14 @@ band = 'K'
 sigma_tt_px = 20
 
 " ARP 194 "
-# fname = 'sample-images/arp194/arp194.fits'
-# angle = 55+90.
-# cropIdx = (1000,500,2500,1500)
+fname = 'sample-images/arp194/arp194.fits'
+angle = 55+90.
+cropIdx = (1000,500,2500,1500)
 
 " ARP 274 "
-fname = 'sample-images/arp274/hlsp_heritage_hst_wfpc2_arp274_f814w_v1_sci.fits'
-angle = 10.
-cropIdx = (500,300,2700,1500)
+# fname = 'sample-images/arp274/hlsp_heritage_hst_wfpc2_arp274_f814w_v1_sci.fits'
+# angle = 10.
+# cropIdx = (500,300,2700,1500)
 
 wfpc2_plate_scale_as_1 = 1.38889E-05 * 3600.
 wfpc2_plate_scale_as_2 = 2.77778E-05 * 3600.
@@ -58,30 +57,22 @@ fitsFileData = getRawImages(fname)
 images_hst = fitsFileData[0]
 
 # 2. Resize and crop to make it look pretty.
-images_hst = rotateAndCrop(images_hst, angle=angle, cropIdx=cropIdx)
+images_hst = rotateAndCrop(images_hst, angle=angle, cropArg=cropIdx)
 image_diffraction_limited = np.fliplr(images_hst)	# Flipping because that's what Rob's one looks like
 
 detector_height_px = image_diffraction_limited.shape[0]
 detector_width_px = image_diffraction_limited.shape[1]
 
-# TODO: resizing the image does something weird to the pixel values
-# image_diffraction_limited_small = resizeImagesToDetector(images_hst, 
-# 	source_plate_scale_as = plate_scale_as, 
-# 	dest_plate_scale_as = plate_scale_as * 2, 
-# 	dest_detector_size_px = (detector_height_px/2, detector_width_px/2),
-# 	plotIt=True)
-
 # 3. Seeing-limit the image by convolving with a Gaussian with a specified FWHM.
-# image_seeing_limited = getSeeingLimitedImage(image_diffraction_limited_small, seeing_diameter_as, plate_scale_as * 2, plotIt=True)
-image_seeing_limited = getSeeingLimitedImage(image_diffraction_limited, seeing_diameter_as, plate_scale_as, plotIt=True)
+# image_seeing_limited = getSeeingLimitedImage(image_diffraction_limited, seeing_diameter_as, plate_scale_as, plotIt=True)
 
 # 4. Generate N images with randomised tip and tilt.
-images_tt, tt_idx = addTurbulence(image_seeing_limited, 
-	N = N_exp, 
-	sigma_tt_px = sigma_tt_px)
+# images_tt, tt_idx = addTurbulence(image_seeing_limited, N_tt = N_exp, sigma_tt_px = sigma_tt_px)
+images_tt, tt_idxs = addTurbulence(image_diffraction_limited, N_tt = N_exp, sigma_tt_px = sigma_tt_px)
+images_noisy_tt, etc_output = addNoise(images_tt, band = band, t_exp = t_exp, plotIt = True)
 
 # 5. Shift-and-stack the images.
-image_stacked = shiftAndStack(images_tt, image_ref=image_seeing_limited, plotIt=False)
+image_stacked = shiftAndStack(images = images_noisy_tt, image_ref=image_diffraction_limited, plotIt=False)
 
 # Plotting
 subplot_x = 3
