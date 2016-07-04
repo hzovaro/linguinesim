5bd159631b50bb1c893684087a3191e042567752
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-04 16:30:36 +1000

    Figured out how to add blur and PSFs in quadrature

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -29,101 +29,66 @@
 ############################################################################################
 #
 #	TO DO:
-#	- Check consistency of np.log usage!
-#	- better way of calculating b_n
-#	- Ultimate goal: what is the minimum surface brightness of galaxies that we can reliably
-#	image with LINGUINI?
 #
 ############################################################################################
 from __future__ import division
 from apdsim import *
 from cosmo_calc import *
-# plt.close('all')
+plt.close('all')
 
-# Simulating an image of a galaxy with a Sersic profile.
-# Disc: truncated exponential.
-# Bulge: de Vaucoeleurs profile. 
+imsize = 501
+image_truth = np.zeros((imsize, imsize))
+image_truth[imsize//2+1, imsize//2+1] = 1
 
-" Inputs "
-# Galaxy:
-R_e = 4	# Half-light or effective radius (kpc) - that is, the radius enclosing half the total light from the galaxy
-R_trunc = 50	# Truncation radius of disc
-mu_e = 15	# Surface brightness at the half-light radius (AB mag/arcsec^2)
-n = 4		# Sersic index
-z = 0.095	# Redshift
-i_deg = 45		# inclination (degrees)
-theta_deg = 0	# rotation (degrees)
-alpha = 30	# rotation (degrees)
-band = 'K'
+sigma_1 = 25
+sigma_2 = 30
+sigma_3 = np.sqrt(sigma_1*sigma_1 + sigma_2*sigma_2)
 
-# In units of kpc...
-R, dR, I_map, mu_map = sersic2D(n=n, R_e=R_e, R_trunc=R_trunc, mu_e=mu_e, plotIt=True, 
-	gridsize=1000,
-	i_rad=np.deg2rad(i_deg), 
-	theta_rad=np.deg2rad(theta_deg))
+image_1 = getSeeingLimitedImage(images = image_truth, seeing_diameter_as = sigma_1, plotIt = False)
+image_2 = getSeeingLimitedImage(images = image_truth, seeing_diameter_as = sigma_2, plotIt = False)
 
-# Converting to units of pixels on our detector...
-D_A_Mpc = distances(z)['D_A_Mpc']
-# Angle (in arcsec) that each pixel on our imaginary detector subtends on the sky at redshift z.
-plate_scale_as = np.rad2deg(2 * np.arctan(dR / 2 / (D_A_Mpc * 1e3))) * 3600
+image_3 = getSeeingLimitedImage(images = image_1, seeing_diameter_as = sigma_2, plotIt = False)
 
-# Resizing to detector AFTER converting to electron count
-flux_map = getElectronCount(mu = mu_map, 
-	band = band,
-	A_tel = telescope.A_collecting,
-	plate_scale_as_px = plate_scale_as,
-	tau = telescope.tau,
-	qe = detector.qe,
-	gain = detector.gain,
-	magnitudeSystem = 'AB'
-	)
+image_4 = getSeeingLimitedImage(images = image_truth, seeing_diameter_as = sigma_3, plotIt = False)
 
-# Resizing to detector BEFORE converting to electron count
-mu_map_apd = resizeImagesToDetector(images_raw = mu_map, 
-	source_plate_scale_as = plate_scale_as, 
-	dest_detector_size_px = (detector.height_px, detector.width_px),
-	dest_plate_scale_as = SYSTEM_PLATE_SCALE_AS_PX)
+image_1 /= max(image_1.flatten())
+image_2 /= max(image_2.flatten())
+image_3 /= max(image_3.flatten())
+image_4 /= max(image_4.flatten())
 
-flux_map_apd_before = getElectronCount(mu = mu_map_apd, 
-	band = band,
-	A_tel = telescope.A_collecting,
-	plate_scale_as_px = SYSTEM_PLATE_SCALE_AS_PX,
-	tau = telescope.tau,
-	qe = detector.qe,
-	gain = detector.gain,
-	magnitudeSystem = 'AB'
-	)
+plt.figure()
+plt.plot(image_1[imsize//2,:], 'm')
+plt.plot(image_2[imsize//2,:], 'k')
+plt.plot(image_4[imsize//2,:], 'b')
+plt.plot(image_3[imsize//2,:], 'r')
 
-extent_imag_detector_as = [-plate_scale_as*mu_map.shape[1]/2,plate_scale_as*mu_map.shape[1]/2,-plate_scale_as*mu_map.shape[0]/2,plate_scale_as*mu_map.shape[0]/2]
-extent_apd_detector_as = [-SYSTEM_PLATE_SCALE_AS_PX*mu_map_apd.shape[1]/2,SYSTEM_PLATE_SCALE_AS_PX*mu_map_apd.shape[1]/2,-SYSTEM_PLATE_SCALE_AS_PX*mu_map_apd.shape[0]/2,SYSTEM_PLATE_SCALE_AS_PX*mu_map_apd.shape[0]/2]
+# Same but in the diffraction limit...
+efl = 50
+wavelength_1 = 500e-9
+wavelength_2 = 800e-9
+D_1 = 0.3
+D_2 = 0.4
+fwhm_1 = wavelength_1 / D_1
+fwhm_2 = wavelength_2 / D_2
+f_ratio_1 = efl / D_1
+f_ratio_2 = efl / D_2
 
-# PLOTTING FLUX AND MU MAPS ON EACH DETECTOR
-plt.figure(figsize=(2*FIGSIZE, 2*FIGSIZE))
-plt.subplot(2,2,1)
-plt.imshow(mu_map, extent = extent_imag_detector_as)
-plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title(r'$\mu(R)$ (imaginary detector)')
-plt.xlabel(r'arcsec')
-plt.ylabel(r'arcsec')
-plt.subplot(2,2,2)
-plt.imshow(mu_map_apd, extent = extent_apd_detector_as)
-plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title(r'$\mu(R)$ (eAPD detector)')
-plt.xlabel(r'arcsec')
-plt.ylabel(r'arcsec')
-plt.subplot(2,2,3)
-plt.imshow(flux_map, extent = extent_imag_detector_as)
-plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title(r'Electron count (imaginary detector)')
-plt.xlabel(r'arcsec')
-plt.ylabel(r'arcsec')
-plt.subplot(2,2,4)
-plt.imshow(flux_map_apd_before, extent = extent_apd_detector_as)
-plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title(r'Electron count (eAPD detector)')
-plt.xlabel(r'arcsec')
-plt.ylabel(r'arcsec')
+fwhm_3 = np.sqrt(fwhm_2*fwhm_2 - fwhm_1*fwhm_1)
+wavelength_3 = wavelength_1
+D_3 = wavelength_3 / fwhm_3
+f_ratio_3 = efl / D_3
+
+image_1 = getDiffractionLimitedImage(image_truth, f_ratio = f_ratio_1, detector_size_px = (imsize, imsize), l_px_m = 5e-6, wavelength = wavelength_1, plotIt = True)
+image_2 = getDiffractionLimitedImage(image_truth, f_ratio = f_ratio_2, detector_size_px = (imsize, imsize), l_px_m = 5e-6, wavelength = wavelength_2, plotIt = True)
+
+image_3 = getDiffractionLimitedImage(image_1, f_ratio = f_ratio_3, detector_size_px = (imsize, imsize), l_px_m = 5e-6, wavelength = wavelength_3, plotIt = True)
+	
+image_1 /= max(image_1.flatten())
+image_2 /= max(image_2.flatten())
+image_3 /= max(image_3.flatten())
+
+plt.figure()
+plt.plot(image_1[imsize//2,:], 'm')
+plt.plot(image_2[imsize//2,:], 'k')
+plt.plot(image_3[imsize//2,:], 'r')
 plt.show()
-
-# exportFITSFile(image_in_array = flux_map_apd_before, fname = 'flux_data', clobber = True, headerData = {'EXPTIME' : 1.0})
-
