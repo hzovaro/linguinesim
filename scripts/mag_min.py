cc367d9f8273ef277e252b2d8c1459a87554e263
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-11 16:46:17 +1000

    First commit

---
+++ scripts/mag_min.py
@@ -0,0 +1,33 @@
+############################################################################################
+#
+# 	File:		mu_min.py
+#	Author:		Anna Zovaro
+#	Email:		anna.zovaro@anu.edu.au
+#
+#	Description:
+#	Determining the maximum magnitude of stars that can be reliably imaged with
+#	LINGUINI.
+#
+#	Copyright (C) 2016 Anna Zovaro
+#
+############################################################################################
+#
+#	This file is part of lignuini-sim.
+#
+#	lignuini-sim is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	lignuini-sim is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with lignuini-sim.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################################
+from __future__ import division
+from apdsim import *
+plt.close('all')
