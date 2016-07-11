a79a47c8475f9b83b9421da28b408b4552f04c84
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-11 16:45:24 +1000

    Added routine for simulating a field of stars

--- apdsim/starsim.py
+++ apdsim/starsim.py
@@ -29,3 +29,72 @@
 ############################################################################################
 from __future__ import division
 from apdsim import *
+
+# Inputs:
+#	- star magnitude & magnitude system
+#	- star coordinates (in arcsec relative to corner of image)
+# Outputs:
+#	- an image of the star as formed by the telescope and detector pair.
+
+def getStarField(m, coords, A_tel, f_ratio, l_px_m, detector_size_px,
+	tau = 1,
+	qe = 1,
+	gain = 1,
+	magnitudeSystem = None,
+	wavelength_m = None, 
+	bandwidth_m = None, 
+	band = None, 
+	plotIt=False):
+	
+	# Figuring out the value of the pixel:
+	# If we were imaging onto our detector with an infinitely large telescope, then all of the light falling on the telescope aperture would be imaged onto a single pixel.
+	# So, we need to get the flux (in units of electrons per pixel) given the magnitude of the star.
+	# Then, we make a normalised PSF grid (s.t. P = 1) with the appropriate plate scale, and simply multiply this by the flux. 
+	# THEN we convert to an expected count.
+	N_stars = len(m)
+	if len(m) != coords.shape[0]:
+		print 'ERROR: the length of the star magnitudes vector must be equal to that of the first dimension of the coords array!'
+		return
+
+	# PSF.
+	detector_height_px, detector_width_px = detector_size_px[0:2]
+	if wavelength_m == None and band != None:
+		wavelength_m = FILTER_BANDS_M[band][0]
+		bandwidth_m = FILTER_BANDS_M[band][1]
+	psf = pointSpreadFunction(wavelength_m, f_ratio, l_px_m, tuple(2*x for x in detector_size_px), plotIt=True)
+	# 'Truth' image (telescope with infinite resolution)
+	image_truth = np.zeros(detector_size_px)
+
+	for k in range(N_stars):
+		# Total expected electron count from the star.
+		Sigma_electrons = surfaceBrightness2countRate(mu = m[k], A_tel = A_tel, tau = tau, qe = qe, gain = gain, magnitudeSystem = magnitudeSystem, wavelength_m = wavelength_m, bandwidth_m = bandwidth_m)		
+		image_truth[coords[k,0], coords[k,1]] = Sigma_electrons
+
+	# Convolving the truth image with the PSF.
+	image_expected = signal.fftconvolve(psf, image_truth, mode='same')[detector_height_px//2 + detector_height_px%2:detector_height_px+detector_height_px//2 + detector_height_px%2 - 1, detector_width_px//2 + detector_width_px%2:detector_width_px+detector_width_px//2 + detector_width_px%2 - 1]
+	if min(image_expected.flatten()) < 0:
+		print 'WARNING: the result of the convolution contains negative pixels!'
+		image_expected -= min(image_expected.flatten())
+	image_count = expectedCount2count(image_expected)
+
+	if plotIt:
+		plt.figure(figsize=(3*FIGSIZE,FIGSIZE))
+		plt.suptitle('Starfield')
+		plt.subplot(1,3,1)
+		plt.imshow(image_truth, norm=LogNorm())
+		plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
+		plt.title('Truth image')
+		plt.subplot(1,3,2)
+		plt.imshow(image_expected, norm=LogNorm())
+		plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
+		plt.title('Expected electron flux')
+		plt.subplot(1,3,3)
+		plt.imshow(image_count, norm=LogNorm())
+		plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
+		plt.title('Count')
+		plt.show()
+
+	return image_count, image_expected, image_truth
+
+
+
