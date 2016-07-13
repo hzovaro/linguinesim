2b956c2feb00000304aa4fd1fa72b14860f562c4
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-13 16:50:42 +1000

    Added routine for printing the shifting-and-stacking alignment errors

--- apdsim/lisim.py
+++ apdsim/lisim.py
@@ -37,8 +37,9 @@
 #	You should have received a copy of the GNU General Public License
 #	along with lignuini-sim.  If not, see <http://www.gnu.org/licenses/>.
 #
-#########################################################################################################
+####################################################################################################
 from __future__ import division
+from __future__ import print_function
 from apdsim import *
 
 def addTurbulence(images, N_tt, sigma_tt_px,
@@ -47,7 +48,7 @@ def addTurbulence(images, N_tt, sigma_tt_px,
 		Add turbulence to an input `truth' image. Returns N_tt copies of the input image with randomised turbulence added. 
 		Just tip and tilt for now with a standard deviation of sigma_tt_px in both dimensions.
 	"""
-	print "Adding randomised tip/tilt to image(s)..."
+	print("Adding randomised tip/tilt to image(s)", end="")
 
 	# Tip and tilt for now	
 	images, N, height, width = getImageSize(images)
@@ -56,7 +57,8 @@ def addTurbulence(images, N_tt, sigma_tt_px,
 	if crop_tt == None:
 		images_tt = np.ndarray((N, N_tt, height, width))
 	else:
-		if type(crop_tt) == int:
+		# if type(crop_tt) == int:
+		if len(crop_tt.shape) == 0:
 			images_tt = np.ndarray((N, N_tt, height - 2 * crop_tt, width - 2 * crop_tt))	
 		else:
 			images_tt = np.ndarray((N, N_tt, height - 2 * crop_tt[0], width - 2 * crop_tt[1]))
@@ -66,6 +68,7 @@ def addTurbulence(images, N_tt, sigma_tt_px,
 	
 	for k in range(N):
 		for j in range(N_tt):
+			print('.', end="")
 			shift_height = np.ceil(np.random.randn() * sigma_tt_px).astype(int)
 			shift_width = np.ceil(np.random.randn() * sigma_tt_px).astype(int)
 			image_tt = shift(images[k], (shift_height, shift_width))
@@ -76,10 +79,10 @@ def addTurbulence(images, N_tt, sigma_tt_px,
 				images_tt[k,j] = image_tt
 			else:
 				images_tt[k,j] = rotateAndCrop(image_tt, angle=0., cropArg=crop_tt)
+		print('\n')
+	return np.squeeze(images_tt), np.squeeze(tt_idxs)
 
-	return (np.squeeze(images_tt), tt_idxs)
-
-#########################################################################################################
+####################################################################################################
 def shiftAndStack(images, 
 	image_ref=None,
 	N=None, 
@@ -88,7 +91,7 @@ def shiftAndStack(images,
 	""" 
 		Shift and stack the images given in the 3-dimensional array of N images. 
 	"""
-	print "Shifting and stacking images..."
+	print("Shifting and stacking images", end="")
 
 	# Error checking
 	# Need to convert to float if necessary.
@@ -98,12 +101,12 @@ def shiftAndStack(images,
 		image_ref = image_ref.astype(np.float64)
 
 	if len(images.shape) > 4:
-		print "WARNING: for now, please only input a 3D array of images to shift and stack! I'm operating on the first set of images..."
+		print("WARNING: for now, please only input a 3D array of images to shift and stack! I'm operating on the first set of images...")
 		images = np.squeeze(images[0])	
 	
 	if len(images.shape) == 3:
 		if N > images.shape[0]:
-			print "ERROR: if specified, N must be equal to or less than the length of the first dimension of the images array."
+			print("ERROR: if specified, N must be equal to or less than the length of the first dimension of the images array.")
 			return -1
 		if image_ref == None:
 			# If the reference image is not specified, we use the first image in the array as the reference: 
@@ -117,7 +120,7 @@ def shiftAndStack(images,
 				N = images.shape[0]			
 	else:
 		# Error: cannot shift and stack a single image!
-		print "ERROR: cannot shift and stack a single image! Input array must have N > 1."
+		print("ERROR: cannot shift and stack a single image! Input array must have N > 1.")
 		return -1
 	
 	image_stacked = np.copy(image_ref)		# shifted-and-stacked image
@@ -130,6 +133,7 @@ def shiftAndStack(images,
 	img_peak_idxs = np.ndarray((N, 2))		# shift in x and y computed from the x-correlation
 
 	for k in range(N):
+		print('.', end="")
 		# Cross-correlate image k with the reference image to find the tip and tilt.
 		corrs[k] = signal.fftconvolve(image_ref, images[k][::-1,::-1])
 		corr_peak_idxs[k] = np.unravel_index(np.argmax(corrs[k]), (2 * height - 1, 2 * width - 1))
@@ -171,23 +175,26 @@ def shiftAndStack(images,
 			# plt.scatter([peak_idxs[k][0]], [peak_idxs[k][1]], c='r', s=20)
 			plt.draw()
 			plt.pause(1)
+	print('\n')
 
 	if plotIt:
 		plt.figure(figsize=(2*FIGSIZE,FIGSIZE))
 		plt.suptitle('Lucky imaging technique')
 		plt.subplot(1,2,1)
-		plt.imshow(images[0], vmin=min(images[0].flatten()), vmax=max(image_stacked.flatten()))
+		# plt.imshow(images[0], vmin=min(images[0].flatten()), vmax=max(image_stacked.flatten()))
+		plt.imshow(images[0], norm=LogNorm())
 		plt.title('Single exposure')
 		plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
 		plt.subplot(1,2,2)
-		plt.imshow(image_stacked, vmin=min(images[0].flatten()), vmax=max(image_stacked.flatten()))
+		# plt.imshow(image_stacked, vmin=min(images[0].flatten()), vmax=max(image_stacked.flatten()))
+		plt.imshow(image_stacked, norm=LogNorm())
 		plt.title('Shifted-and-stacked image')
 		plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
 		plt.show()
 
-	return image_stacked
+	return image_stacked, img_peak_idxs
 
-#########################################################################################################
+####################################################################################################
 def luckyImaging(images, type, vararg):
 	""" 
 		Apply a Lucky Imaging (LI) technique to a sequence of images stored in the input array images. 
@@ -199,3 +206,28 @@ def luckyImaging(images, type, vararg):
 
 
 	return image_lucky
+
+####################################################################################################
+def alignmentError(in_idxs, out_idxs,
+	verbose=True):
+	"""
+		Compute the alignment errors arising in the Lucky Imaging shifting-and-stacking process given an input array of tip and tilt coordinates applied to the input images and the coordinates of the shifts applied in the shifting-and-stacking process. 
+	"""
+	N = in_idxs.shape[0]
+	errs = np.zeros((N))
+	n_errs = 0
+		
+	for k in range(N):
+		errs[k] = np.sqrt(np.power(in_idxs[k,0] - out_idxs[k,0],2) + np.power(in_idxs[k,1] - out_idxs[k,1],2))
+		if errs[k] > 0:
+			n_errs += 1
+			
+	if verbose:
+		print('------------------------------------------------')
+		print('Tip/tilt coordinates\nInput\t\tOutput\t\tError')
+		print('------------------------------------------------')
+		for k in range(N):
+			print('(%6.2f,%6.2f)\t(%6.2f,%6.2f)\t%4.2f' % (in_idxs[k,0],in_idxs[k,1],out_idxs[k,0],out_idxs[k,1],errs[k]))
+		print('------------------------------------------------')
+
+	return n_errs, errs
