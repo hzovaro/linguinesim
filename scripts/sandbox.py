0995af0c3aecfd68e78c4e42b6ea09e30c80e42d
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-14 14:59:16 +1000

    Script for converting pixel intensities to counts using the trapezoidal rule

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -30,30 +30,91 @@
 from __future__ import division
 from apdsim import *
 from cosmo_calc import *
+from scipy import integrate
+from mpl_toolkits.mplot3d import Axes3D
 plt.close('all')
+ 
+# First: 2D case. Then move onto 3D case.
 
-# Input parameters
-N_stars = 5
-N_tt = 5
-t_exp = 10e-3
-band = 'K'
-sigma_tt_px = 20
-crop_tt = 50
-m_max = 20
-m_min = 10
+# PROBLEM: the Airy function is given in units of intensity I(x, y) (in photons/s/m^2). We, however, need the photon count rate (in photons/s). So we need to integrate the Airy function over each pixel to get the count rate. 
+# This needs to be done in 2D.
 
-# Make a star field.
-image_count, starfield_padded, m, coords = getStarField(N_stars = N_stars, m_min = m_min, m_max = m_max, A_tel = telescope.A_collecting, f_ratio = telescope.f_ratio, l_px_m = detector.l_px_m, detector_size_px = tuple(2*crop_tt+x for x in detector.size_px), magnitudeSystem = 'AB', band = band, plotIt = True)
-starfield = starfield_padded[crop_tt:crop_tt+detector.height_px,crop_tt:crop_tt+detector.width_px]
+# Using a lambda function for the Airy disc:
 
-# Add tip and tilt.
-starfields_tt, in_idxs = addTurbulence(starfield_padded, N_tt, sigma_tt_px, crop_tt)
+# 1. Make a function for the integrand. 
+wavelength_m = FILTER_BANDS_M['K'][0]
+f_ratio = telescope.f_ratio
+# Total intensity (W)
+P_0 = 1
+# Central intensity (W m^-2)
+I_0 = P_0 * np.pi / 4 / wavelength_m / wavelength_m / f_ratio / f_ratio
 
-# Add noise.
-starfields_noisy, etc_output = addNoise(starfields_tt, band = band, t_exp = t_exp)
 
-# Applying the shift-and-stack routine.
-starfield_stacked, out_idxs = shiftAndStack(starfields_tt, image_ref = starfield, plotIt = True)
+# Calculating the PSF
+# x, y will be in units of metres
+r = lambda x, y: np.pi / wavelength_m / f_ratio * np.sqrt(np.power(x,2) + np.power(y,2))
+I = lambda x, y : np.power((2 * special.jv(1, r(x,y)) / r(x,y)), 2) * I_0 
 
-# Printing the alignment error.
-printAlignmentError(in_idxs, out_idxs)
+# 2. Calculate the sum of the integral for each pixel.
+# Sanity check: over the infinite plane. Should get back P_0.
+# P = integrate.nquad(I, [[-100*l_px_m, +100*l_px_m], [-100*l_px_m, +100*l_px_m]])
+# print P
+
+# Evaluating I for a grid.
+l_px_m = detector.l_px_m
+oversampleFactor = 4
+sz_x = 3
+sz_y = 3
+detector_height_px = sz_x * oversampleFactor
+detector_width_px = sz_y * oversampleFactor
+# We add one extra row and column for the trapezoidal integration method.
+x = np.arange(-detector_height_px//2, +detector_height_px//2 + detector_height_px%2 + 1, 1) + 0.
+y = np.arange(-detector_width_px//2, +detector_width_px//2 + detector_width_px%2 + 1, 1) + 0.
+x *= l_px_m / oversampleFactor
+y *= l_px_m / oversampleFactor
+Y, X = np.meshgrid(y, x)
+
+I_grid = I(X,Y)
+nan_idx = np.where(np.isnan(I_grid))
+if nan_idx[0].shape != (0,):
+	I_grid[nan_idx[0][0],nan_idx[1][0]] = I_0 # removing the NaN in the centre of the image if necessary
+
+# Numerically integrating using Simpson's (or trapezoidal) rule:
+# In x first, then in y.
+# First, just integrate over the whole field to see if it returns the expected vale.
+res = integrate.cumtrapz(I_grid, dx = l_px_m/oversampleFactor, axis = 0, initial = 0)
+res2 = integrate.cumtrapz(res[-1,:], dx = l_px_m/oversampleFactor, initial = 0)
+print 'Total count using cumtrapz:', res2[-1]
+print 'Total count using I * A_px:',sum(I_grid.flatten())*l_px_m**2/oversampleFactor**2
+
+# Now, we want to repeat this process for each pixel.
+photon_grid = np.zeros((sz_x,sz_y))
+cumsum = 0
+for j in range(sz_y):
+	for k in range(sz_x):
+		px_grid = I_grid[oversampleFactor*k:oversampleFactor*k+oversampleFactor+1,oversampleFactor*j:oversampleFactor*j+oversampleFactor+1]
+		# Checking
+		cumsum += sum(px_grid.flatten())*l_px_m**2/oversampleFactor**2
+		res1 = integrate.cumtrapz(px_grid, dx = l_px_m/oversampleFactor, axis = 0, initial = 0)
+		res2 = integrate.cumtrapz(res1[-1,:], dx = l_px_m/oversampleFactor, initial = 0)
+		photon_grid[k,j] = res2[-1]
+print 'Total count after oversampling and numerical integration:',sum(photon_grid.flatten())
+print 'Total count using sum(px_grid * A_px):', cumsum
+
+# Comparing...
+plt.figure(figsize=(3*FIGSIZE,FIGSIZE))
+plt.subplot(1,3,1)
+plt.imshow(I_grid)
+plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
+plt.title('Intensity')
+plt.subplot(1,3,2)
+plt.imshow(photon_grid)
+plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
+plt.title('Pixel counts')
+plt.subplot(1,3,3)
+plt.imshow(I_grid*l_px_m**2/oversampleFactor**2)
+plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
+plt.title('Oversampled approximated pixel counts')
+plt.show()
+
+count_cumtrapz, I, P_0, P_sum, I_0 = airyDisc(wavelength_m = wavelength_m, f_ratio = f_ratio, l_px_m = l_px_m, detector_size_px = detector.size_px, plotIt = True)
