cca9495e52e0b6f998450616a23d0c99c50ceefd
Anna Zovaro <azovaro@Annas-dator.local>
2016-07-15 16:27:06 +1000

    Implemented script for debugging 2D Gaussian fitting process

--- scripts/sandbox.py
+++ scripts/sandbox.py
@@ -29,92 +29,44 @@
 ############################################################################################
 from __future__ import division
 from apdsim import *
-from cosmo_calc import *
-from scipy import integrate
-from mpl_toolkits.mplot3d import Axes3D
+from astropy.modeling import models, fitting
 plt.close('all')
- 
-# First: 2D case. Then move onto 3D case.
 
-# PROBLEM: the Airy function is given in units of intensity I(x, y) (in photons/s/m^2). We, however, need the photon count rate (in photons/s). So we need to integrate the Airy function over each pixel to get the count rate. 
-# This needs to be done in 2D.
+height = 256
+width = 320
+I_0 = 1e3
+sigma = 10
+x_0 = 10.1
+y_0 = 30.1
 
-# Using a lambda function for the Airy disc:
+# Sigma?
+# Background noise levels?
 
-# 1. Make a function for the integrand. 
-wavelength_m = FILTER_BANDS_M['K'][0]
-f_ratio = telescope.f_ratio
-# Total intensity (W)
-P_0 = 1
-# Central intensity (W m^-2)
-I_0 = P_0 * np.pi / 4 / wavelength_m / wavelength_m / f_ratio / f_ratio
+Y, X = np.mgrid[-height/2:height/2, -width/2:width/2]
+# Y, X = np.mgrid[:height, :width]
 
+# Z = I_0 * np.exp(-((X - x_0)**2 + (Y - y_0)**2) / (2 * sigma**2) )
+image_1 = getStarField(m = 10, coords = np.array((height/2, width/2)), t_exp = 10e-3, A_tel = telescope.A_collecting, f_ratio = telescope.f_ratio*20, l_px_m = detector.l_px_m, detector_size_px = detector.size_px, magnitudeSystem = 'AB', band = 'K', plotIt = False, tau = telescope.tau, gain = detector.gain, qe = detector.qe, detectorSaturation = detector.saturation)[1]
+image_2 = shift(image_1, (x_0, y_0))
+# image_2 = np.copy(image_1)
+corr = signal.fftconvolve(image_1, image_2[::-1,::-1],'same')
+corr = corr / max(corr.flatten())
+# corr = corr[height/2-1:3*height/2-1,width/2-1:3*width/2-1]
 
-# Calculating the PSF
-# x, y will be in units of metres
-r = lambda x, y: np.pi / wavelength_m / f_ratio * np.sqrt(np.power(x,2) + np.power(y,2))
-I = lambda x, y : np.power((2 * special.jv(1, r(x,y)) / r(x,y)), 2) * I_0 
+p_init = models.Gaussian2D(x_stddev=1.,y_stddev=1.)
+fit_p = fitting.LevMarLSQFitter()
+p_fit = fit_p(p_init, X, Y, corr)
 
-# 2. Calculate the sum of the integral for each pixel.
-# Sanity check: over the infinite plane. Should get back P_0.
-# P = integrate.nquad(I, [[-100*l_px_m, +100*l_px_m], [-100*l_px_m, +100*l_px_m]])
-# print P
+max_idx = np.array((p_fit.x_mean.value, p_fit.y_mean.value))
+print("Indices of maximum in Gaussian fit relative to image centre: (%5.2f,%5.2f)" % (max_idx[0], max_idx[1]))
 
-# Evaluating I for a grid.
-l_px_m = detector.l_px_m
-oversampleFactor = 4
-sz_x = 3
-sz_y = 3
-detector_height_px = sz_x * oversampleFactor
-detector_width_px = sz_y * oversampleFactor
-# We add one extra row and column for the trapezoidal integration method.
-x = np.arange(-detector_height_px//2, +detector_height_px//2 + detector_height_px%2 + 1, 1) + 0.
-y = np.arange(-detector_width_px//2, +detector_width_px//2 + detector_width_px%2 + 1, 1) + 0.
-x *= l_px_m / oversampleFactor
-y *= l_px_m / oversampleFactor
-Y, X = np.meshgrid(y, x)
-
-I_grid = I(X,Y)
-nan_idx = np.where(np.isnan(I_grid))
-if nan_idx[0].shape != (0,):
-	I_grid[nan_idx[0][0],nan_idx[1][0]] = I_0 # removing the NaN in the centre of the image if necessary
-
-# Numerically integrating using Simpson's (or trapezoidal) rule:
-# In x first, then in y.
-# First, just integrate over the whole field to see if it returns the expected vale.
-res = integrate.cumtrapz(I_grid, dx = l_px_m/oversampleFactor, axis = 0, initial = 0)
-res2 = integrate.cumtrapz(res[-1,:], dx = l_px_m/oversampleFactor, initial = 0)
-print 'Total count using cumtrapz:', res2[-1]
-print 'Total count using I * A_px:',sum(I_grid.flatten())*l_px_m**2/oversampleFactor**2
-
-# Now, we want to repeat this process for each pixel.
-photon_grid = np.zeros((sz_x,sz_y))
-cumsum = 0
-for j in range(sz_y):
-	for k in range(sz_x):
-		px_grid = I_grid[oversampleFactor*k:oversampleFactor*k+oversampleFactor+1,oversampleFactor*j:oversampleFactor*j+oversampleFactor+1]
-		# Checking
-		cumsum += sum(px_grid.flatten())*l_px_m**2/oversampleFactor**2
-		res1 = integrate.cumtrapz(px_grid, dx = l_px_m/oversampleFactor, axis = 0, initial = 0)
-		res2 = integrate.cumtrapz(res1[-1,:], dx = l_px_m/oversampleFactor, initial = 0)
-		photon_grid[k,j] = res2[-1]
-print 'Total count after oversampling and numerical integration:',sum(photon_grid.flatten())
-print 'Total count using sum(px_grid * A_px):', cumsum
-
-# Comparing...
-plt.figure(figsize=(3*FIGSIZE,FIGSIZE))
-plt.subplot(1,3,1)
-plt.imshow(I_grid)
+plt.figure(figsize=(2*FIGSIZE,FIGSIZE))
+plt.subplot(1,2,1)
+plt.imshow(corr)
 plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title('Intensity')
-plt.subplot(1,3,2)
-plt.imshow(photon_grid)
+plt.title('Cross-correlation')
+plt.subplot(1,2,2)
+plt.imshow(p_fit(X,Y))
 plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title('Pixel counts')
-plt.subplot(1,3,3)
-plt.imshow(I_grid*l_px_m**2/oversampleFactor**2)
-plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
-plt.title('Oversampled approximated pixel counts')
+plt.title('Model fit')
 plt.show()
-
-count_cumtrapz, I, P_0, P_sum, I_0 = airyDisc(wavelength_m = wavelength_m, f_ratio = f_ratio, l_px_m = l_px_m, detector_size_px = detector.size_px, plotIt = True)
