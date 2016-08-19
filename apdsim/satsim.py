a0502de0f7cfcd7a9646db7ee9a4edf1f8b8a610
Anna Zovaro <azovaro@Annas-dator.local>
2016-08-19 10:00:50 +1000

    First commit

---
+++ apdsim/satsim.py
@@ -0,0 +1,31 @@
+############################################################################################
+#
+# 	File:		galsim.py
+#	Author:		Anna Zovaro
+#	Email:		anna.zovaro@anu.edu.au
+#
+#	Description:
+#	For simulating images of galaxies. 
+#
+#	Copyright (C) 2016 Anna Zovaro
+#
+############################################################################################
+#
+#	This file is part of linguinesim.
+#
+#	linguinesim is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	linguinesim is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with linguinesim.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################################
+from __future__ import division
+from linguinesim.apdsim import *
