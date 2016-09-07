72989a6f9d9f53764e36a0a153838f5fb9089fa8
Anna Zovaro <azovaro@Annas-dator.local>
2016-09-07 15:45:56 +1000

    Changed to import new classes

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 # Importing classes.
 from telescopeclass import TelescopeClass as Telescope 
 from detectorclass import DetectorClass as Detector
+from cryostatclass import CryostatClass as Cryostat
+from opticalsystemclass import OpticalSystemClass as OpticalSystem
+from skyclass import SkyClass as Sky
