80f0642ff9fcb64232fa061f1436403e83211dd1
Anna Zovaro <azovaro@Annas-dator.local>
2016-09-09 16:49:24 +1000

    First commit

---
+++ galaxyclass.py
@@ -0,0 +1,53 @@
+############################################################################################
+#
+# 	File:		galaxy.py
+#	Author:		Anna Zovaro
+#	Email:		anna.zovaro@anu.edu.au
+#
+#	Description:
+#	A class for a galaxy.
+#
+#	Copyright (C) 2016 Anna Zovaro
+#
+#########################################################################################################
+#
+#	This file is part of apd-sim.
+#
+#	apd-sim is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	apd-sim is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with apd-sim.  If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################################################
+from __future__ import division, print_function
+
+####################################################################################################
+class Galaxy(object):
+
+	def __init__(self, name, R_e_as, mu_e, sersic_idx,
+		axis_ratio=1,
+		theta_pa_deg=0,
+		z=None,
+		masses_solar=None,
+		coords=None,
+		gama_id=None):
+
+		self.name = name
+		self.R_e_as = R_e_as
+		self.mu_e = mu_e
+		self.sersic_idx = sersic_idx
+		self.axis_ratio = axis_ratio
+		self.theta_pa_deg = theta_pa_deg
+		self.z = z
+		self.masses_solar = masses_solar
+
+		self.coords = coords
+		self.gama_id = gama_id
