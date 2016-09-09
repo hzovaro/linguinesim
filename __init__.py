b0dcf0d93150296be91edc776bfc4dd0af98a0ef
Anna Zovaro <azovaro@Annas-dator.local>
2016-09-09 16:51:19 +1000

    Removed all package imports to modules

--- __init__.py
+++ __init__.py
@@ -1,6 +1,46 @@
-# Importing classes.
-from telescopeclass import TelescopeClass as Telescope 
-from detectorclass import DetectorClass as Detector
-from cryostatclass import CryostatClass as Cryostat
-from opticalsystemclass import OpticalSystemClass as OpticalSystem
-from skyclass import SkyClass as Sky
+####################################################################################################
+#   ________  ________  ________  ________  ___  _____ ______      
+#  |\   __  \|\   __  \|\   ___ \|\   ____\|\  \|\   _ \  _   \    
+#  \ \  \|\  \ \  \|\  \ \  \_|\ \ \  \___|\ \  \ \  \\\__\ \  \   
+#   \ \   __  \ \   ____\ \  \ \\ \ \_____  \ \  \ \  \\|__| \  \  
+#    \ \  \ \  \ \  \___|\ \  \_\\ \|____|\  \ \  \ \  \    \ \  \ 
+#     \ \__\ \__\ \__\    \ \______/|____\_\  \ \__\ \__\    \ \__\
+#      \|__|\|__|\|__|     \|______/\__________\|__|\|__|     \|__|
+#                                   \|_________|                   
+#
+#	File:		__init__.py
+#	Author:		Anna Zovaro
+#	Email:		anna.zovaro@anu.edu.au
+#
+#	Description:
+#	A package for simulating Lucky Imaging of Nearby Galaxies Undertaken In the Near-Infrared (LINGUINe)
+#
+#	Copyright (C) 2016 Anna Zovaro
+#
+####################################################################################################
+#
+#	This file is part of linguinesim.
+#
+#	linguinesim is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	linguinesim is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with linguinesim.  If not, see <http://www.gnu.org/licenses/>.
+#
+####################################################################################################
+# __all__ = ["etc", "etcutils", "fftwconvolve", "galsim", "imutils", "linguineglobals", "lisim", "obssim", "ossim", "satsim", "starsim"]
+
+# Classes
+from telescopeclass import Telescope
+from detectorclass import Detector
+from cryostatclass import Cryostat
+from opticalsystemclass import OpticalSystem
+from skyclass import Sky
+from galaxyclass import Galaxy
