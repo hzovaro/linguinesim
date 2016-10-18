a15b50a1fcd96801afdd6c96266da8dc7d524393
Anna Zovaro <azovaro@Annas-dator.local>
2016-10-18 11:30:37 +1100

    Added routine to generate a seeing_limited_system instance for the ANU 2.3m telescope-SAPHIRA detector system

--- ossim.py
+++ ossim.py
@@ -37,13 +37,15 @@ from cryostatclass import Cryostat
 from opticalsystemclass import OpticalSystem
 from skyclass import Sky
 from galaxyclass import Galaxy
+from aosim.pyxao import wavefront, deformable_mirror, wfs, ao_system, atmosphere, seeing_limited_system
+
 import etc
 
 ############################################################################################
 def aoiOpticalSystem():
 
 	opticalsystem = OpticalSystem(
-		telescope = eos18mTelescope(),
+		telescope = eos18mTelescope(),	
 		detector = nuvuDetector(),
 		sky = msoSky(),
 		plate_scale_as_px = 0.044
@@ -101,13 +103,96 @@ def msoSky():
 		)
 
 ############################################################################################
-def aoiAoSystem():
+def aoiAoSystem(wave_height_px,
+	wavelength_wfs_m = 589e-9,
+	wavelength_science_m = 800e-9,
+	rng_seed = 1
+	):
 	"""
 		Make an AO system instance for AOI.
 	"""
-	
+	wavefrontPupil = {	
+		'type':'annulus',
+		'dout': 1.752,
+		'din' : 0.250
+	}
 
-############################################################################################
+	# Wave parameters
+	m_per_px = wavefrontPupil['dout'] / wave_height_px		# Physical mapping of wave onto primary mirror size
+
+	# Imaging parameters
+
+	# AO system parameters
+	N_actuators = 17
+	N_lenslets = 16
+	actuator_pitch_m = wavefrontPupil['dout'] / N_actuators
+	lenslet_pitch_m = wavefrontPupil['dout'] / N_lenslets
+	ho_loop_rate = 2000
+
+	dm_geometry = 'square'
+	wfs_geometry = 'square'
+	central_actuator = 'true'
+	central_lenslet = 'false'
+	
+	edge_radius = 1.4	
+	influence_fun = 'gaussian'
+	pokeStroke = 1e-7	
+
+	# Atmospheric conditions at MSO
+	wavelength_ref_m = 500e-9		# Wavelength reference for Fried parameter
+	r0_ref_m = 5e-2
+	r0_wfs = np.power((wavelength_wfs_m / wavelength_ref_m), 1.2) * r0_ref_m
+	r0_science = np.power((wavelength_science_m / wavelength_ref_m), 1.2) * r0_ref_m 
+
+	v_wind_m = 10			# Turbulent layer wind speed (m/s)
+	wind_angle_deg = 0.0	# Turbulent layer wind direction (rad)
+	elevation_m = 1000		# Turbulent layer elevation (m)
+	airmass = 1.0			# Airmass
+
+	# Setting up AO system
+	wf_wfs = wavefront.Wavefront(wave = wavelength_wfs_m, m_per_px = m_per_px, sz = wave_height_px, pupil = wavefrontPupil)
+	wf_science = wavefront.Wavefront(wave = wavelength_science_m, m_per_px = m_per_px, sz = wave_height_px, pupil = wavefrontPupil)
+	wavefronts_dm = [wf_wfs, wf_science] 	# Wavefronts corrected by the DM (in a CL AO system, it's all of them!)
+	wavefronts_wfs = [wf_wfs]				# Wacefronts sensed by the WFS
+	psf_ix = 1		# Index in the list of wavefronts passed to the DM instance corresponding to the PSF to return
+
+	dm = deformable_mirror.DeformableMirror(
+		wavefronts = wavefronts_dm, 
+		influence_function = 'gaussian', 
+		central_actuator = central_actuator, 
+		actuator_pitch = actuator_pitch_m, 
+		geometry = dm_geometry, 
+		edge_radius = 1.4)
+
+	sh_wfs = wfs.ShackHartmann(
+		wavefronts = wavefronts_wfs, 
+		lenslet_pitch = lenslet_pitch_m, 
+		geometry = wfs_geometry, 
+		central_lenslet = central_lenslet, 		
+		sampling = 1)
+		# fratio = wfs_fratio)
+
+	aoi_ao_system = ao_system.SCFeedBackAO(dm = dm, wfs = sh_wfs, image_ixs = psf_ix)
+	
+	# The atmosphere is a PHASE SCREEN
+	atm = atmosphere.Atmosphere(sz = wave_height_px, 
+		m_per_px = m_per_px,
+		elevations = elevation_m, 
+		r_0 = r0_ref_m, 
+		wave_ref = wavelength_ref_m, 
+		angle_wind = wind_angle_deg,
+		v_wind = v_wind_m, 
+		airmass = airmass, 
+		seed = rng_seed)
+
+	wf_wfs.add_atmosphere(atm)
+	wf_science.add_atmosphere(atm)
+
+	aoi_ao_system.response_matrix = np.load("aoi_response_matrix.npz")['response_matrix']
+	aoi_ao_system.reconstructor = np.load("aoi_reconstructor_matrix.npz")['reconstructor']
+
+	return aoi_ao_system
+
 ############################################################################################
 def anu23mTelescope():
 	"""
@@ -208,3 +293,64 @@ def linguineOpticalSystem():
 		cryostat = saphiraCryostat(),
 		sky = ssoSky()
 		)
+
+############################################################################################
+def linguineAoSystem(wave_height_px,
+	rng_seed = 1
+	):
+	"""
+		Make an AO system instance for the SAPHIRA-2.3 m telescope system. This is only used to generate seeing-limited and diffraction-limited PSFs as the telescope doesn't have an AO system.
+	"""
+	wavefront_pupil = {	
+		'type':'annulus',
+		'dout': 2.337,
+		'din' : 0.335,
+	}
+
+	# Wave parameters
+	m_per_px = wavefront_pupil['dout'] / wave_height_px		# Physical mapping of wave onto primary mirror size
+
+	# Atmospheric conditions at SSO
+	wavelength_ref_m = 500e-9		# Wavelength reference for Fried parameter
+	r0_ref_m = 10e-2
+
+	v_wind_m = 10			# Turbulent layer wind speed (m/s)
+	wind_angle_deg = 0.0	# Turbulent layer wind direction (rad)
+	elevation_m = 1000		# Turbulent layer elevation (m)
+	airmass = 1.0			# Airmass
+
+	wavelengths_science_m = []
+	wavelength_ixs = {}
+	r0_science_m = []
+	wavefronts = []
+	bands = ['J', 'H', 'K']
+	for k in range(len(bands)):
+		band = bands[k]
+		wavelength_ixs[band] = k
+		wavelengths_science_m.append(FILTER_BANDS_M[band][0])
+		r0_science_m.append(np.power((FILTER_BANDS_M[band][0] / wavelength_ref_m), 1.2) * r0_ref_m)
+		# Making a wavefront instance
+		wavefronts.append(
+			wavefront.Wavefront(wave = FILTER_BANDS_M[band][0],
+				m_per_px = m_per_px,
+				sz = wave_height_px,
+				pupil = wavefront_pupil)
+			)	
+	
+	# The atmosphere is a PHASE SCREEN: not dependent on wavelegnth!
+	atm = atmosphere.Atmosphere(sz = wave_height_px, 
+		m_per_px = m_per_px,
+		elevations = elevation_m, 
+		r_0 = r0_ref_m, 
+		wave_ref = wavelength_ref_m, 
+		angle_wind = wind_angle_deg,
+		v_wind = v_wind_m, 
+		airmass = airmass, 
+		seed = rng_seed)
+
+	for w in wavefronts:
+		w.add_atmosphere(atm)
+
+	linguine_ao_system = seeing_limited_system.SeeingLimitedOpticalSystem(wavefronts = wavefronts, wavelength_ixs = wavelength_ixs)
+
+	return linguine_ao_system
